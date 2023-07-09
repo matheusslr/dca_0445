d67f79bb182f41e5961d8776b8d0cd006068b039
matheusslr <matheusslr@hotmail.com>
2023-07-09 08:44:04 -0300

    fix: filter now works fine

--- unidade 02/filtro_homomorfico/homomorfic_filter.py
+++ unidade 02/filtro_homomorfico/homomorfic_filter.py
@@ -1,87 +1,78 @@
 import cv2
 import numpy as np
 
-def homomorphic_filter(image, cutoff_freq, c, gammaH, gammaL):
-    # Convertendo a imagem para escala de cinza
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    
-    # Normalizando a imagem de entrada entre 0 e 1
-    normalized = gray / 255.0
-    
-    # Aplicando a transformada de log para obter a imagem no domínio da frequência
-    spectrum = np.log1p(np.abs(np.fft.fftshift(np.fft.fft2(normalized))))
-    
-    # Definindo os parâmetros do filtro homomórfico
-    D0 = cutoff_freq  # Frequência de corte
-    
-    # Calculando a máscara do filtro homomórfico
-    rows, cols = spectrum.shape
-    center_row, center_col = int(rows / 2), int(cols / 2)
-    y = np.arange(-center_row, rows - center_row)
-    x = np.arange(-center_col, cols - center_col)
-    X, Y = np.meshgrid(x, y)
-    D = np.sqrt(X**2 + Y**2)
-    H = (gammaH - gammaL) * (1 - np.exp(-c * (D**2) / (D0**2))) + gammaL
-    
-    # Aplicando o filtro no domínio da frequência
-    filtered_spectrum = np.exp(np.fft.ifft2(np.fft.ifftshift(spectrum * H)))
-    
-    # Normalizando a imagem filtrada para o intervalo [0, 255]
-    filtered_image = np.uint8(filtered_spectrum.real * 255)
-    
-    return filtered_image, H
+def deslocaDFT(image):
+    cx = image.shape[1] // 2
+    cy = image.shape[0] // 2
+    image[:cy, :cx], image[cy:, cx:] = image[cy:, cx:].copy(), image[:cy, :cx].copy()
+    image[cy:, :cx], image[:cy, cx:] = image[:cy, cx:].copy(), image[cy:, :cx].copy()
 
-def on_cutoff_trackbar(value):
-    global cutoff_freq
-    cutoff_freq = value
+def on_trackbar(value, param):
+    pass
 
-def on_c_trackbar(value):
-    global c
-    c = value / 100.0
+def main():
+    image = cv2.imread(r'unidade 02\filtro_homomorfico\img\ceu_escuro.jpg', cv2.IMREAD_GRAYSCALE)
 
-def on_gammah_trackbar(value):
-    global gammaH
-    gammaH = value / 100.0
+    # Calcula os tamanhos ótimos para o cálculo da DFT
+    dft_M = cv2.getOptimalDFTSize(image.shape[0])
+    dft_N = cv2.getOptimalDFTSize(image.shape[1])
 
-def on_gammal_trackbar(value):
-    global gammaL
-    gammaL = value / 100.0
+    # Realiza o padding da imagem
+    padded = cv2.copyMakeBorder(image, 0, dft_M - image.shape[0], 0, dft_N - image.shape[1], cv2.BORDER_CONSTANT, value=0)
 
-# Carregando a imagem
-image = cv2.imread(r'unidade 02\filtro_homomorfico\img\ceu_escuro.jpg')
+    # Cria a matriz temporária para o filtro
+    tmp = np.zeros((dft_M, dft_N), dtype=np.float32)
 
-# Inicializando os valores dos parâmetros
-cutoff_freq = 10
-c = 1.0
-gammaH = 1.5
-gammaL = 0.5
+    gammaH = 15.0
+    gammaL = 1.0
+    c = 1.0
+    D0 = 20
 
-# Criando a janela do OpenCV
-cv2.namedWindow('Filtro Homomórfico')
-cv2.namedWindow('Filtro')
+    cv2.namedWindow('Filtro Homomórfico')
 
-# Criando os trackbars
-cv2.createTrackbar('Cutoff', 'Filtro Homomórfico', cutoff_freq, 50, on_cutoff_trackbar)
-cv2.createTrackbar('c', 'Filtro Homomórfico', int(c * 100), 200, on_c_trackbar)
-cv2.createTrackbar('gammaH', 'Filtro Homomórfico', int(gammaH * 100), 200, on_gammah_trackbar)
-cv2.createTrackbar('gammaL', 'Filtro Homomórfico', int(gammaL * 100), 200, on_gammal_trackbar)
+    cv2.createTrackbar('gammaH', 'Filtro Homomórfico', int(gammaH * 10), 100, on_trackbar)
+    cv2.createTrackbar('gammaL', 'Filtro Homomórfico', int(gammaL * 10), 100, on_trackbar)
+    cv2.createTrackbar('c', 'Filtro Homomórfico', int(c * 100), 100, on_trackbar)
+    cv2.createTrackbar('D0', 'Filtro Homomórfico', D0, 100, on_trackbar)
 
-while True:
-    # Aplicando o filtro homomórfico
-    filtered_image, H = homomorphic_filter(image, cutoff_freq, c, gammaH, gammaL)
-    
-    # Normalizando a imagem do filtro para o intervalo [0, 255]
-    filtered_image_norm = (H - np.min(H)) / (np.max(H) - np.min(H))
-    filtered_image_norm = np.uint8(filtered_image_norm * 255)
-    
-    # Exibindo a imagem original, a imagem filtrada e a imagem do filtro
-    cv2.imshow('Imagem original', image)
-    cv2.imshow('Imagem filtrada', filtered_image)
-    cv2.imshow('Filtro', filtered_image_norm)
-    
-    # Verificando se a tecla 'Esc' foi pressionada
-    key = cv2.waitKey(1) & 0xFF
-    if key == 27:
-        break
+    while True:
+        # Atualiza os parâmetros do filtro homomórfico
+        gammaH = cv2.getTrackbarPos('gammaH', 'Filtro Homomórfico') / 10.0
+        gammaL = cv2.getTrackbarPos('gammaL', 'Filtro Homomórfico') / 10.0
+        c = cv2.getTrackbarPos('c', 'Filtro Homomórfico') / 100.0
+        D0 = cv2.getTrackbarPos('D0', 'Filtro Homomórfico')
 
-cv2.destroyAllWindows()
+        # Preenche a matriz temporária
+        for i in range(dft_M):
+            for j in range(dft_N):
+                tmp[i, j] = (gammaH - gammaL) * (1.0 - np.exp(-1.0 * c * ((((i - dft_M // 2) ** 2) + ((j - dft_N // 2) ** 2)) / (D0 ** 2)))) + gammaL
+
+        # Calcula a DFT
+        complexImage = cv2.dft(np.float32(padded), flags=cv2.DFT_COMPLEX_OUTPUT)
+
+        # Realiza a troca de quadrantes
+        deslocaDFT(complexImage)
+
+        # Aplica o filtro frequencial
+        filtered = cv2.mulSpectrums(complexImage, cv2.merge([tmp, tmp]), 0)
+
+        # Troca novamente os quadrantes
+        deslocaDFT(filtered)
+
+        # Calcula a DFT inversa
+        idft = cv2.idft(filtered, flags=cv2.DFT_SCALE | cv2.DFT_REAL_OUTPUT)
+
+        filtered_image = cv2.normalize(idft, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+
+        cv2.imshow('Imagem original', image)
+        cv2.imshow('Imagem filtrada', filtered_image)
+
+        # Verifica se a tecla 'Esc' foi pressionada
+        key = cv2.waitKey(1) & 0xFF
+        if key == 27:
+            break
+
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
